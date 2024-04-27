f9ccda3518ac184491c872327acd28007d6116ff
Kunj2103 <kunjjansari1@gmail.com>
2024-04-27 07:28:06 +0530

    Added header and hero page

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import About from "@/components/About";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+      {/* Header */}
+      <Header />
+      {/* Hero */}
+      <section id="hero" className="snap-start">
+        <Hero />
+      </section>
+      {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
+
+      {/* Experience */}
+      {/* Skills */}
+      {/* Projects */}
+      {/* Contact Me */}
+    </main>
+  );
+}
