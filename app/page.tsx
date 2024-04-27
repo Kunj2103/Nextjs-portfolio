f06ddfb980e18f363cfa575a0628ee0a93b66e43
Kunj2103 <kunjjansari1@gmail.com>
2024-04-27 23:35:11 +0530

    Added projects page with static data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
 
@@ -28,6 +29,10 @@ export default function Home() {
       </section>
 
       {/* Projects */}
+      <section id="projects" className="snap-center">
+        <Projects />
+      </section>
+
       {/* Contact Me */}
     </main>
   );
